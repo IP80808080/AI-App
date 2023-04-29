21b631c3d3dd240f629c5b1ebb815bf6887c671f
KamleshKasambe <kasambe.kamlesh1@gmail.com>
2023-04-29 18:10:16 +0530

    Hey there meet my new AI Assistant.

--- lib/constant/api_consts.dart
+++ lib/constant/api_consts.dart
@@ -1,2 +1,2 @@
 String BASE_URL = "https://api.openai.com/v1";
-String API_KEY = "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";
+String API_KEY = "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";
